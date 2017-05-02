0d9750efedfcf175869b59d9876d638e9e37ade3
Ryan Walker <ryanwalker7@gmail.com>
2017-05-02 06:55:58 -0700

    circleci and heroku integration

---
+++ src/test/java/com/ryanwalker/domain/YNABReportTest.java
@@ -0,0 +1,15 @@
+package com.ryanwalker.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class YNABReportTest {
+
+    @Test
+    public void addTransactionToReport() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+}
